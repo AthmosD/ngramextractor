358c01fc4d85515d267dfca8050987c9a50d35da
AthmosD <78855036+AthmosD@users.noreply.github.com>
2021-10-24 15:43:26 +0200

    Added plots and more ml algos

--- ngramextractor.py
+++ ngramextractor.py
@@ -7,6 +7,9 @@ import collections
 import random
 from itertools import product
 from collections import Counter
+from collections import OrderedDict
+from heapq import nlargest
+
 from nltk.util import ngrams
 import numpy as np
 import pandas as pd
@@ -29,6 +32,12 @@ from sklearn.metrics import r2_score
 from elftools.elf.elffile import ELFFile
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
+import itertools
+from itertools import dropwhile
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 def elffilesorter():
     target = "benign_samples/ubiquiti/"
@@ -63,7 +72,7 @@ def extract_ngrams(directory = "sample/", n_size = 1):
     benign_filelist = os.listdir(benign_directory)
     malware_filelist = os.listdir(malware_directory)
 
-    sample_size = int(min(len(benign_filelist),len(malware_filelist)))-990
+    sample_size = int(min(len(benign_filelist),len(malware_filelist))-1950)
     
     benign_files = random.sample(benign_filelist, sample_size)
     malware_files = random.sample(malware_filelist, sample_size)
@@ -75,7 +84,8 @@ def extract_ngrams(directory = "sample/", n_size = 1):
     keyslist = set([])
     ngramdictlist = []
     isMalware = []
-    countersum = {}
+    #countersum = collections.defaultdict(int)
+    countersum = Counter()
 
     for filename in samplefiles:
         #Read a file:
@@ -91,29 +101,77 @@ def extract_ngrams(directory = "sample/", n_size = 1):
 
         #Count occurences of unique ngrams:
         vector_ngram = dict(Counter(ngramslist))
-        countersum =+ Counter(ngramslist)
+        countersum += Counter(ngramslist)
         ngramdictlist.append(vector_ngram)
-
-        #Add new keys to a list:
-        for val in vector_ngram.keys():
-            keyslist.add(val)
         filecount += 1
-        print(filecount, "files extracted.")
+        print(filecount, "Files done.")
 
 
+    print(countersum)
+    input("COUNTERSUM")
+    print("Amount of keys: ", len(countersum.keys()))
+    print("Amount of values: ", len(countersum.values()))
+
+    print(list(countersum.values())[:10])
+
+    #ThreeHighest = nlargest(10, countersum, key = countersum.get)
+
+    #df = pd.DataFrame([countersum.values()], ["Occurences"], countersum.keys())
+    #df = pd.DataFrame(dict(countersum), index=[0])
+    #df = pd.DataFrame(countersum, columns=countersum.keys(), index=[0])
+    #df.plot(kind='barh')
+
+    toplist = countersum.most_common(len(countersum))
+    topkeylist = list()
+    topvallist = list()
+    for i in range(len(toplist)):
+        topkeylist.append(toplist[i][0])
+        topvallist.append(toplist[i][1])
+    for i in range(len(toplist)):
+        print(topkeylist[i],":" ,topvallist[i])
+
+    keylist = list()
+    for key in topkeylist:
+        keylist.append(str(key))
+
+    params = {'figure.figsize'  : (100, 90),
+          'axes.labelsize'  : 20,
+          'axes.titlesize'  : 20,
+          'xtick.labelsize' : 10,
+          'ytick.labelsize' : 5} 
+
+    plt.rcParams.update(params)
+
+    plt.barh(keylist[1:100], topvallist[1:100], height = 0.5, align = 'center')
+ 
+    plt.xlabel('Occurence inside the dataset')
+    plt.ylabel('N-gram sample')
+    plt.show()
+
+    cntcountersumvefore = len(countersum)
+
+    countersum = Counter({k: countersum for k, countersum in countersum.items() if countersum >= 100})
+
+
+    print(countersum)
+    print("COUNTERSUM NO VALS UNDER x, keys:", len(countersum), "<-", cntcountersumvefore)
+    input("")
+    
     emptyset = []
-    for val in countersum.keys():
+    for val in list(countersum.keys()):
         emptyset.append(val)
     emptyset.sort()
 
     print(len(emptyset))
     print(emptyset)
 
-
     #Generate empty dictionary based on every occured ngram:
     keyslist = list(keyslist)
     keyslist.sort()
     print(len(keyslist))
+
+    keyslist = emptyset
+
     ngram_empty = {}
     for val in keyslist:
         ngram_empty[val] = 0
@@ -126,12 +184,15 @@ def extract_ngrams(directory = "sample/", n_size = 1):
 
     for val in ngramdictlist:
         ngram_base = ngram_empty
-        ngram_base.update(val)
+        #ngram_base.update(val)
+        for key in ngram_base:
+            if key in val:
+                ngram_base[key] = val[key]
         ngrams_final.append(list(ngram_base.values()))
         filecount += 1
         print(filecount, "ngrams updated and appended to final list. Ngram vector lenght:", len(list(ngram_base.values())))
+        del(val)
 
-    print(ngramdictlist, file = open("ngramdictlist.txt","w"))
     input("ENTER")
 
     filem = open("malware_" + str(n_size) + "grams.txt", "w")
@@ -179,7 +240,7 @@ def extract_header(filename):
 
 
 #Setting of n_size and target directory:
-n_size = 4
+n_size = 2
 target_directory = "sample/arm/"
 keylist = 0 
 
@@ -211,15 +272,29 @@ X, y = dataset, datasetMB
 kf = KFold(n_splits=10)
 
 regressor = RandomForestClassifier(max_depth=2, random_state=0)
-for train_index, test_index in kf.split(X):
 
-    x_train, x_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+rfc = RandomForestClassifier(max_depth=2, random_state=0)
+knc = KNeighborsClassifier(n_neighbors=3)
+svc = make_pipeline(StandardScaler(), SVC(gamma='auto'))
 
-    regressor.fit(x_train,y_train)
-    prediction = regressor.predict(x_test)
+mlalgorithms = [rfc, knc, svc]
+conf_mtxs = list()
 
-    print('TN/FP/FN/TP', confusion_matrix(y_test, prediction).ravel())
-    print('\n')
+print("Number of folds: ", KFold.n_splits)
+
+for mlalgo in mlalgorithms:
+    for train_index, test_index in kf.split(X):
+
+        x_train, x_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        mlalgo.fit(x_train,y_train)
+        prediction = mlalgo.predict(x_test)
+
+        print(type(mlalgo))
+        print('TN/FP/FN/TP', confusion_matrix(y_test, prediction).ravel())
+        conf_mtxs.append(confusion_matrix(y_test, prediction))
+        print('\n')
+        
 
 
